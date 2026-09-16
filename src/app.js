ae0073f1edf12d80ca6816fca58e10c955e5ae39
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2026-09-16 13:17:01 +0300

    start the server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+
+
+// Middlewares
+app.use(express.json())
+
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+
+
+
+
+module.exports = app
