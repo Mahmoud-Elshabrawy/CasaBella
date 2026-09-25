1ced2fe97a044340d505205f853af608d6c9149e
Mahmoud Elshabrawy <mahmoudwaell391@gmail.com>
2026-09-26 01:48:32 +0300

    feat: implement user profile and admin user management

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRoutes = require("./authRoutes");
+const userRoutes = require("./userRoutes")
 const mainCategoriesRoutes = require("./mainCategoriesRoutes");
 const categoriesRoutes = require("./categoryRoutes");
 const productsRoutes = require("./productRoutes");
@@ -7,6 +8,7 @@ const productsRoutes = require("./productRoutes");
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
 router.use("/main-categories", mainCategoriesRoutes);
 router.use("/categories", categoriesRoutes);
 router.use("/products", productsRoutes);
